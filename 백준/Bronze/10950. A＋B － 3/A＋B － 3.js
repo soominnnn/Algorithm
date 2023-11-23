778ba727c140501a7303d4202154ef2cdf424088
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-11-23 23:19:51 +0900

    [Bronze V] Title: A+B - 3, Time: 188 ms, Memory: 9724 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,24 @@
+function solution(input) {
+  for(let i = 1; i <= input[0]; i++) {
+    console.log(input[i][0] + input[i][1]);
+  }
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,    
+});
+
+let input = [];
+let list = [];
+
+rl.on("line", function (line) {
+   input.push(line)
+}).on("close", function () {
+    input.forEach((val) => {
+       list.push(val.split(' ').map((el) => parseInt(el)));
+    });
+    solution(list);
+    process.exit();
+});
