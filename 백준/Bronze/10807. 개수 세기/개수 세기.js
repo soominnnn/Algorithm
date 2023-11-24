58e8f09b4a010ff5e061cd85b68a8e5d80444840
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-11-24 16:11:13 +0900

    [Bronze V] Title: 개수 세기, Time: 160 ms, Memory: 9464 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.js
@@ -0,0 +1,24 @@
+function solution(input) {
+  const FIND_NUMBER = Number(input[2]);
+  let result = input[1].filter(el => el === FIND_NUMBER).length;
+  console.log(result);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,    
+});
+
+let input = [];
+let list = [];
+
+rl.on("line", function (line) {
+   input.push(line)
+}).on("close", function () {
+    input.forEach((val) => {
+       list.push(val.split(' ').map((el) => parseInt(el)));
+    });
+    solution(list);
+    process.exit();
+});
