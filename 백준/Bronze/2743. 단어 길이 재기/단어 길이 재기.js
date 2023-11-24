cb278573b4f767f44aa6e3c023705e5263657cb4
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-11-24 17:09:30 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 144 ms, Memory: 9440 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.js
@@ -0,0 +1,20 @@
+function solution(input) {
+  console.log(input);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+        output: process.stdout,
+});
+
+let input;
+
+rl.on("line", function (line) {
+    input = line.length; 
+    rl.close();
+}).on("close", function () {
+
+    solution(input);
+    process.exit();
+});
