8e099a870a46653412ef41c7507c1bcdf4ff5431
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-11-27 23:28:39 +0900

    [Bronze V] Title: 아!, Time: 148 ms, Memory: 9448 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.js
@@ -0,0 +1,19 @@
+function solution(input) {
+    let result = input[0].length >= input[1].length ? 'go' : 'no';
+    console.log(result);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,    
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+   input.push(line)
+}).on("close", function () {
+    solution(input);
+    process.exit();
+});
