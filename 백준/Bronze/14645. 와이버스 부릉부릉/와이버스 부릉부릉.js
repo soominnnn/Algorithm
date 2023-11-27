2f300f163b34f84f3540ba64995ef9b2367b37a9
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-11-27 23:53:13 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 148 ms, Memory: 9436 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.js
@@ -0,0 +1,18 @@
+function solution(input) {
+    console.log("비와이");
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,    
+});
+
+let input = [];
+
+rl.on("line", function (line) {
+   input.push(line)
+}).on("close", function () {
+    solution(input);
+    process.exit();
+});
