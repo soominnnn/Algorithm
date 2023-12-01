fb74ae7db8bb7f2fe3e2292769a22c7faab01cca
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-12-01 19:52:05 +0900

    [Bronze II] Title: 단어의 개수, Time: 240 ms, Memory: 23188 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,21 @@
+function solution(input) {
+    const STRING = input.trim().split(' ');
+    const RESULT = STRING.filter(el => el !== '').length;
+    console.log(RESULT);  
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,    
+});
+
+let input;
+
+rl.on("line", function (line) {
+    input = line;
+    rl.close();
+}).on("close", function () {
+    solution(input);
+    process.exit();
+});
