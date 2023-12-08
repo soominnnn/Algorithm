2814e6e72c9569fa56c8c184a45e6080213734ec
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-12-09 00:02:54 +0900

    [Bronze IV] Title: R2, Time: 144 ms, Memory: 9452 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.js
@@ -0,0 +1,23 @@
+function solution(input) {
+    const RESULT = input[1] * 2 - input[0];
+    console.log(RESULT);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,    
+});
+
+let input;
+let list = [];
+
+rl.on("line", function (line) {
+    input = line;
+    rl.close();
+}).on("close", function () {
+    list = input.split(' ').map((el) => Number(el));
+
+    solution(list);
+    process.exit();
+});
