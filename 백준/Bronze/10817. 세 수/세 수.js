6cd620efb2c18693820eadec3b86d9acb503c76c
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-12-12 00:54:43 +0900

    [Bronze III] Title: 세 수, Time: 160 ms, Memory: 9468 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.js
@@ -0,0 +1,24 @@
+function solution(input) {
+    const SORT_ARRAY = input.sort((a,b) => a - b);
+    
+    console.log(SORT_ARRAY[1]);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,    
+});
+
+let input;
+let list = [];
+
+rl.on("line", function (line) {
+    input = line;
+    rl.close();
+}).on("close", function () {
+    list = input.split(' ').map((el) => Number(el));
+
+    solution(list);
+    process.exit();
+});
