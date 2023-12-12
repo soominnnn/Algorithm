e85d0cc1fc9332e814d660f6f7d5a6b79b73a6a9
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-12-12 21:14:27 +0900

    [Bronze IV] Title: Next in line, Time: 144 ms, Memory: 9440 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.js
@@ -0,0 +1,24 @@
+function solution(input) {
+    const DIFFERENCE = input[1] - input[0];
+
+    console.log(Number(input[1]) + DIFFERENCE);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,    
+});
+
+let input = [];
+let list = [];
+
+rl.on("line", function (line) {
+   input.push(line)
+}).on("close", function () {
+    input.forEach((val) => {
+       list.push(val.split(' ').map((el) => parseInt(el)));
+    });
+    solution(list);
+    process.exit();
+});
