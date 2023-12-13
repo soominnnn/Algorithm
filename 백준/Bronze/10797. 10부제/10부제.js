3c0ca2efdbd647be2120e870d834bbfffa0598ec
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2023-12-13 21:26:00 +0900

    [Bronze IV] Title: 10부제, Time: 160 ms, Memory: 9456 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.js
@@ -0,0 +1,22 @@
+function solution(input) {
+    console.log(input[1].filter(el => el === Number(input[0])).length);   
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,    
+});
+
+let input = [];
+let list = [];
+
+rl.on("line", function (line) {
+   input.push(line)
+}).on("close", function () {
+    input.forEach((val) => {
+       list.push(val.split(' ').map((el) => parseInt(el)));
+    });
+    solution(list);
+    process.exit();
+});
