2caa5c42fa37819a234fdc16afbcc30222a34e06
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-17 19:16:32 +0900

    [level 0] Title: 접미사 배열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    const result = [];
+    
+    for(let i = 0; i < my_string.length; i++) {
+        result.push(my_string.slice(i, my_string.length));
+    }
+    
+    return result.sort();
+}
