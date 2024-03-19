d52208ed2ed491aba77fbea914949a3f88733d88
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-03-19 20:29:53 +0900

    [level 0] Title: 리스트 자르기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.js
@@ -0,0 +1,17 @@
+function solution(n, slicer, num_list) {
+    switch(n) {
+        case 1: {
+            return num_list.slice(0, slicer[1] + 1);
+        }
+        case 2: {
+            return num_list.slice(slicer[0], num_list.length);
+        }
+        case 3: {
+            return num_list.slice(slicer[0], slicer[1] + 1);
+        }
+        case 4: {
+            return num_list.slice(slicer[0], slicer[1] + 1)
+                           .filter((_, index) => index % 2 === 0);
+        }
+    }
+}
