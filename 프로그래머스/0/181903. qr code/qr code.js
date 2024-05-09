373ab47b7468e086b938bb208120ee64509a7c44
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2024-05-09 10:43:30 +0900

    [level 0] Title: qr code, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.js
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -1,12 +1,6 @@
 function solution(q, r, code) {
-    const splitArray = code.split('');
-    let result = '';
-    
-    for(let i = 0; i < splitArray.length; i++) {
-        if(i % q === r) {
-            result += splitArray[i];
-        }
-    }
+    const array = code.split('').filter((_, index) => index % q === r);
+    const result = array.join('');
     
     return result;
 }
