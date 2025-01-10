e623ce0b9327fbe204c34e8953c249683b0a1e4a
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-01-10 20:13:00 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{   
+    int score[5];
+    int sum = 0;
+    
+    for(int i = 0; i < 5; i++) {
+        cin >> score[i];
+        
+        if(score[i] < 40) {
+            score[i] = 40;
+        }
+        
+        sum += score[i];
+    }
+    
+    cout << sum / 5;
+
+    return 0;
+}
