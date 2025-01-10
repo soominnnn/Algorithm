ee75a1c61bd8b42a89871d4d50725c14c9a9bafd
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-01-10 20:33:56 +0900

    [Bronze IV] Title: 세수정렬, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{   
+    int arr[3];
+    
+    for (int i = 0; i < 3; i++) {
+        cin >> arr[i];
+    }
+    
+    sort(arr, arr + 2 + 1);
+    
+    for (int i = 0; i < 3; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    return 0;
+}
