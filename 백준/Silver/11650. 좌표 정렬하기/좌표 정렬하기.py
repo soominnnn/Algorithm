4c2df2d87c89383080ff4dae163d9a2f3c934ac9
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-09-10 22:01:25 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 2720 ms, Memory: 46504 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,19 +1,18 @@
 """
 1. 입력 -> O(N): 100,000
-2. 우선순위 큐 사용 -> SORT 혹은 FOR문 사용 시, 시간초과날 확률이 높음
-2. 출력 -> 좌표 순서대로
+2. FOR문 사용 시, 시간초과날 확률이 높음
+3. 출력 -> 좌표 순서대로
 """
-from collections import deque
 
-deque = []
+array = []
 N = int(input())
 
 for _ in range(N): # O(N)
     x, y = map(int, input().split())
-    deque.append((x, y))
+    array.append((x, y))
 
-deque.sort(reverse=True)
+array.sort(reverse=True)
 
 for _ in range(N): # O(N)
-    x, y = deque.pop()
+    x, y = array.pop()
     print(x, y)
