b2366a8a1f0e277b50c3ca05b94129bb814316ba
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-10-06 18:31:27 +0900

    [level 0] Title: 문자열 출력하기, Time: 2.74 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    string str;
+    
+    cin >> str;
+    cout << str;
+    
+    return 0;
+}
