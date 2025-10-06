752d803651c085443f11c6326791371fe7114a0e
Soominnnn <105346651+soominnnn@users.noreply.github.com>
2025-10-06 18:34:10 +0900

    [level 0] Title: a와 b 출력하기, Time: 3.23 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a;
+    int b;
+    
+    cin >> a >> b;
+    cout << "a = " << a << endl;
+    cout << "b = " << b;
+    return 0;
+}
